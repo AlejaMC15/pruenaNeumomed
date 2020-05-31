74189d06a919078fa3703d182e76aba1f63e94f6
AlejaMC15 <aleja-mc@hotmail.com>
2020-05-30 20:15:34 -0500

    Vinculación de botón con formulario

--- src/component/header.js
+++ src/component/header.js
@@ -2,30 +2,53 @@ import React from 'react';
 import Logo from './RecursoWeb/Logo-Oficial.png';
 
 function Header() {
-
-    return (
-
-        <header>
-            <div className="container">
-                <nav className="navbar navbar-expand-lg navbar-light">
-                    <a className="navbar-brand" href="...">
-                        <img src={Logo} width="109" height="69" className="d-inline-block align-top img-fluid ml-0" alt="" />
-                    </a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse justify-content-end mr-0" id="navbarNavAltMarkup">
-                        <a className="nav-item nav-link text-primary font-weight-bold" href="...">INICIO <span className="sr-only">(current)</span></a>
-                        <div className="navbar-nav">
-                            <a className="nav-item nav-link text-primary font-weight-bold" href="...">FORMULARIO <span className="sr-only">(current)</span></a>
-                        </div>
-                    </div>
-                </nav>
+  return (
+    <header>
+      <div className="container">
+        <nav className="navbar navbar-expand-lg navbar-light">
+          <a className="navbar-brand" href="...">
+            <img
+              src={Logo}
+              width="109"
+              height="69"
+              className="d-inline-block align-top img-fluid ml-0"
+              alt=""
+            />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="collapse navbar-collapse justify-content-end mr-0"
+            id="navbarNavAltMarkup"
+          >
+            <a
+              className="nav-item nav-link text-primary font-weight-bold"
+              href="..."
+            >
+              INICIO <span className="sr-only">(current)</span>
+            </a>
+            <div className="navbar-nav">
+              <a
+                className="nav-item nav-link text-primary font-weight-bold"
+                href="#form"
+              >
+                FORMULARIO <span className="sr-only">(current)</span>
+              </a>
             </div>
-        </header>
-
-    );
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
-
